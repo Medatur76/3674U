36aea5f29eb4ffc9c088180387e38ee944ec0deb
Kiernan <medaturd76@gmail.com>
2026-09-24 16:40:21 -0500

    We have 4 motor chassis not 6 lol

--- src/setup.cpp
+++ src/setup.cpp
@@ -4,5 +4,5 @@
 
 #include "main.h"
 
-pros::MotorGroup Hardware::left_motors({1, 2, 3});
-pros::MotorGroup Hardware::right_motors({4, 5, 6});
+pros::MotorGroup Hardware::left_motors({1, 2});
+pros::MotorGroup Hardware::right_motors({3, 4});
